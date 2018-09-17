0a383e2ba8ac8cd1036724e812969b1b4bce8ca5
Selenamm <elizabethamirenamiranda@gmail.com>
2018-09-17 15:37:45 -0600

    nada

--- main_game.py
+++ main_game.py
@@ -1,90 +1,311 @@
-userList = [{"name":"Dayanna","nickname":"dram","id": "dram", "password":"123","typeRol":"Administrador"},
-            {"name":"Selena","nickname":"selena","id": "selanamm", "password":"semm","typeRol":"Administrador"}]
+# Guarda la lista de Usuarios registrados.
+usersList = [{"name": "Dayanna", "id": "dram", "password": "123", "typeRol": "Administrador"},
+             {"name":"Selena","nickname":"selena","id": "selena", "password":"1234","typeRol":"Jugador","juegosJugados":0,"puntosGanados":0}]
+
+# Guarda la lista de palabras.
+wordsList = []
+
+# Pide los datos del usuario administrador como su: nombre, identificador y contraseña.
+def registerAdministrator():
+    print("Datos del Administrador:")
+    name = (input("1) Nombre del Administrador: "))
+    id = input("3) Identificador: ")
+    password = (input("4) Contraseña:"))
+    typeRol = "Administrador"
+
+    print("\n")
+
+    registerUserAdministrator(name, id, password, typeRol) # Llama a la función que agrega mediante un diccionario
+                                                           # ususarios administradores.
+    userAdministrator()                                    # Llama a la función de administrar ususarios.
 
 
+# Pide los datos al usuario jugador como su: nombre, alias, identificador y contraseña.
+def registerPlayers():
+    print("Datos del Jugador:")
+    name = (input("1) Nombre del Jugador: "))
+    nickname = (input("2) Nickname/Alias: "))
+    id = input("3) Identificador: ")
+    password = (input("4) Contraseña:"))
+    typeRol = "Jugador"
+
+    print("\n")
+
+    registerUserPlayers(name, nickname, id, password, typeRol) # Llama a la función que agrega mediante un diccionario
+                                                               # ususarios jugadores.
+    userAdministrator()                                        # Llama a la función de administrar ususarios.
 
 
-def registerUser(name,nickname,id,password,typeRol):
+def registerUserPlayers(name, nickname, id, password, typeRol):
     newUser = {}
     newUser["name"] = name
     newUser["nickname"] = nickname
     newUser["id"] = id
     newUser["password"] = password
     newUser["typeRol"] = typeRol
+    newUser["juegosJugados"] = 0
+    newUser["puntosGanados"] = 0
 
-    userList.append(newUser)
+    usersList.append(newUser)
 
+def registerUserAdministrator(name, id, password, typeRol):
+    newUser = {}
+    newUser["name"] = name
+    newUser["id"] = id
+    newUser["password"] = password
+    newUser["typeRol"] = typeRol
 
-def register():
-    name = (input("1) Nombre: "))
-    nickname = (input("2) Nickname: "))
-    id = input("3) ID: ")
-    password = (input("4) Password:"))
-    print("Rol= Administrador o Jugador")
-    typeRol = input(str("Tipo de rol: "))
-
-    registerUser(name,nickname,id,password,typeRol)
-    adminstratorOpcions()
-
+    usersList.append(newUser)
 
 
-def userAdministrator():
-    print("1) Crear usuario\n"
-          "2) Borrar usuario\n"
-          "3) Ver lista de usuarios\n"
-          "4) Regresar")
 
-    opcion = int(input("opcion"))
 
-    if opcion == 1:
-        register()
-    elif opcion == 3:
-        print(userList)
-        adminstratorOpcions()
+def mainLogin():
+    print(">>> Menú de inicio de sesión <<<\n"
+          "Por favor, ingrese su:")
+    iD = input("1) Identificador: ")
+    password = input("2) Contraseña: ")
 
-def adminstratorOpcions():
-    print("1) Administrar usuarios\n"
-          "2) Menu de inicio de secion")
+    print("\n")
 
-    opcion = int(input("opcion: "))
+    loginUser(iD,password)
 
-    if opcion == 1:
-        userAdministrator()
-    elif opcion == 2:
-        mainLogin()
+
 
 
 def loginUser(id,password):
 
-    for user in userList:
+    for user in usersList:
         if user["id"] == id:
             if user["password"] == password:
                 verifyTyoeRol(user)
-            elif user["id"] != id and user["password"] != password:
-                print("Intente de nuevo")
-                intentos = 1
-                while intentos < 4:
-                    print("Lleva " + str(intentos)+ " intentos de 3")
-                    intentos+=1
-                    mainLogin()
+
             else:
                 print("No le quedan intentos")
-                mainLogin()
+                print("\n")
+                mainMenu()
 
-        else:
-            mainLogin()
 def verifyTyoeRol(user):
-    print(user["name"] + ", usted tiene el rol de: " + user["typeRol"] )
-    if user["typeRol"] == "Administrador" or "administrador":
-        adminstratorOpcions()
-    elif user["typeRol"] == "Jugador" or "jugador":
+
+    if user["typeRol"] == "Administrador":
+        print(user["name"] + ", usted tiene el rol de: " + user["typeRol"] + ".")
+        print("\n")
+        administratorMenu()
+    elif user["typeRol"] == "Jugador":
+        print(user["nickname"] + ", usted tiene el rol de: " + user["typeRol"] + ".")
+        print("\n")
+        playerMenu()
+
+
+def mainMenu():
+
+    run_program = True
+
+    while(run_program):
+
+        print("***** Bienvenido al Juego Corrector de Ortografía ***** \n"
+                            "1) Iniciar Sesión.\n"
+                            "2) Salir. ")
+
+        option = input(str("→ Opción: "))
+
+        print("\n")
+
+        if(option == "1"):
+            mainLogin()
+        elif option == "2":
+            run_program = False
+
+
+def addTypeUser():
+    print("¿Qué usuario desea agregar?\n"
+          "1) Administrador.\n"
+          "2) Jugador.\n"
+          "3) Regresar.")
+    option = int(input("→ Usuario: "))
+
+    print("\n")
+
+    if option == 1:
+        registerAdministrator()
+
+    elif option == 2:
+        registerPlayers()
+    elif option == 3:
+        userAdministrator()
+    else:
+        addTypeUser()
+
+
+def removeUsers():
+    count = 1
+    for i in usersList:
+        print(str(count) + ") " + i["name"] + " (" + i["typeRol"] + ").")
+        count += 1
+
+    delete = int(input("→ Digite el número de usuario que desea eliminar: "))
+
+    count2 = 1
+    if delete == count2:
+        usersList.pop(count2 -1)
+        count2 +=1
+
+    print("¡El usuario se eliminó con éxito!")
+    print("\n")
+
+    userAdministrator()
+
+
+def userAdministrator():
+    print("----- Menú de Administrador de Usisarios ------")
+    print("1) Crear usuario.\n"
+          "2) Borrar usuario.\n"
+          "3) Ver lista de usuarios.\n"
+          "4) Regresar al menú del admistrador.")
+
+    option = int(input("→ Opción: "))
+
+    print("\n")
+
+    if option == 1:
+        addTypeUser()
+    elif option == 2:
+        removeUsers()
+    elif option == 3:
+        print(usersList)
+        administratorMenu()
+    elif option == 4:
+        administratorMenu()
+    else:
+        userAdministrator()
+
+
+def seePlayerList():
+    jugador = 1
+    for player in usersList:
+        if player["typeRol"] == "Jugador":
+            print(str(jugador) + ")" + "  Identificador: " + player["id"] + " / " + "Nombre del jugador: " + player["name"]
+                  + " / " + "Nickname: " + player["nickname"] + ".")
+        jugador +=1
+
+    print("\n")
+
+    consultsAdministratror()
+
+def consultsAdministratror():
+    print("----- Menú de Consultas ------")
+    print("1) Ver lista de jugadores.\n"
+          "2) Ver los mejores 10 jugadores.\n"
+          "3) Promedio de juegos jugados.\n"
+          "4) Ver jugadores con menor cantidad de puntos.\n"
+          "5) Regresar al menú administrador.")
+
+    option = int(input("→ Opción: "))
+
+    print("\n")
+
+    if option == 1:
+        seePlayerList()
+    elif option == 2:
+        pass
+    elif option == 3:
+        print(usersList)
+        pass
+    elif option == 4:
+        pass
+    elif option == 5:
+        administratorMenu()
+    else:
+        consultsAdministratror()
+
+
+def administratorMenu():
+    print("----- Menú del Administrador -----")
+    print("1) Administrar usuarios.\n" 
+          "2) Consultas.\n"
+          "3) Cerrar sesión.")
+
+    opcion = input(str("→ Opción: "))
+
+    print("\n")
+
+    if opcion == "1":
+        userAdministrator()
+
+    elif opcion == "2":
+        consultsAdministratror()
+
+    elif opcion == "3" :
+        mainMenu()
+
+    else:
+        administratorMenu()
+
+
+def modifyNickname():
+   pass
+
+
+def userDataLogin():
+    user = mainLogin()
+    for i in usersList:
+        if i["id"] == user:
+            print("Nombre: " + i["name"] + "." +"\n" + "Alias: " + i["nickname"] + "." + "\n" + "Juegos jugados: " + i["juegosJugados"] + "." + "\n" + "Puntos Ganados: " + i["puntosGanados"] + ".")
+
+    consultsplayer()
+
+
+def consultsplayer():
+    print("----- Menú de Consultas ------")
+    print("1) Ver lista de jugadores.\n"
+          "2) Ver datos de la cuenta.\n"
+          "3) Regresar al menú jugador.")
+
+    option = int(input("→ Opción: "))
+
+    print("\n")
+
+    if option == 1:
+        seePlayerList()
+    elif option == 2:
+        userDataLogin()
+    elif option == 3:
+        playerMenu()
+    else:
+        consultsplayer()
+
+
+def playerMenu():
+    print("----- Menú del Jugador -----")
+    print("¿Qué desea realizar?:\n"
+          "1) Jugar.\n"
+          "2) Cambiar el nickname.\n"
+          "3) Consultas.\n"
+          "4) Cerrar sesión.")
+
+
+    opcion = input(str("→ Opción: "))
+
+    print("\n")
+
+    if opcion == "1":
         pass
 
+    elif opcion == "2":
+        pass
 
-def mainLogin():
-    print("Bienvenido al menu de inicio de secion")
-    iD = input("Identificador: ")
-    password = input("Contraseña: ")
-    loginUser(iD,password)
+    elif opcion == "3":
+        consultsplayer()
 
-mainLogin()
+    elif opcion == "4":
+        mainMenu()
+
+    else:
+        playerMenu()
+
+
+
+
+
+
+mainMenu()
